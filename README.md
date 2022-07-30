b0a35e5c7903300c69db8207622528a0bf29b33b
Rodrigo A. Melo <rodrigomelo9@gmail.com>
2022-07-30 17:12:56 -0300

    Initial commit
    Added README.md, LICENSE and the vivado project.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Zynq Petalinux Example
+
+> **WARNING:** it is WIP and I'm learning Petalinux
+
+* Tools version: 2022.1
+* Board: ZCU102 (Zynq US+)
+* Host OS: Ubuntu 20.04
+* License: CC-BY-4.0
+
+# Instructions
+
+## Generate the Hardware Design
+
+* Open the Vivado project `vivado/project.xpr`
+* Generate Bitstream
+* File -> Export -> Export Hardware (include bitstream)
+  * It will generate the needed `.xsa` file
+* Close Vivado
---
+++ vivado/project.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2022.1 (lin64) Build 3526262 Mon Apr 18 15:47:01 MDT 2022
+//Date        : Sat Jul 30 14:57:10 2022
+//Host        : rodrigo-ThinkPad-T14-Gen-1 running 64-bit Ubuntu 20.04.4 LTS
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   ();
+
+
+  design_1 design_1_i
+       ();
+endmodule
